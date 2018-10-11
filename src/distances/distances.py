12166d55a3df6ff6129088e74d6987e335f5e48e
Anders Bogsnes <andersbogsnes@gmail.com>
2018-10-11 10:31:17 +0200

    Added utils

--- src/distances/distances.py
+++ src/distances/distances.py
@@ -2,7 +2,6 @@ from googlemaps import Client
 from .utils import get_api_key
 
 
-
 class Distances:
     def __init__(self):
         self.client = Client(get_api_key())
--- src/distances/utils.py
+++ src/distances/utils.py
@@ -1,10 +1,17 @@
 import os
+import pathlib
 import platform
+
+import pandas as pd
 
 IS_WINDOWS = platform.system() == 'Windows'
 
 
-class APIError(Exception):
+class DistanceAPIError(Exception):
+    pass
+
+
+class DistanceIOError(Exception):
     pass
 
 
@@ -12,6 +19,21 @@ def get_api_key():
     try:
         return os.environ['API_KEY']
     except KeyError:
-        message = f"""Must set API_KEY environment variable - 
-        use {'set' if IS_WINDOWS else 'export'} API_KEY=<your-key>' at the terminal"""
-        raise APIError(message)
+        message = f"""Must set API_KEY environment variable - type
+        '{'set' if IS_WINDOWS else 'export'} API_KEY=<your-key>'
+         at the terminal"""
+        raise DistanceAPIError(message)
+
+
+def read_data(file_path, column_name, **kwargs):
+    file_path = pathlib.Path(file_path)
+
+    if file_path.suffix == '.xlsx' or file_path.suffix == '.xls':
+        df = pd.read_excel(file_path, **kwargs)
+
+    elif file_path.suffix == '.csv':
+        df = pd.read_csv(file_path, **kwargs)
+    else:
+        raise DistanceIOError(f"""{file_path.suffix} is unknown,
+        use either excel or csv formats""")
+    return df[column_name]
